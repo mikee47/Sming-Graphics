fdbd2bc0c752b8dbf3f37522c9fc22ed90fb0b22
mikee47 <mike@sillyhouse.net>
2021-10-14 21:18:12 +0100

    Draw color test as boxes to show display orientation
    Boxes are numbered to help, shows if display is mirrored or normal

--- samples/Color_Test/app/application.cpp
+++ samples/Color_Test/app/application.cpp
@@ -11,7 +11,6 @@ using namespace Graphics;
 
 namespace
 {
-
 RenderQueue renderQueue(tft);
 
 void done(SceneObject* scene)
@@ -21,20 +20,36 @@ void done(SceneObject* scene)
 
 void render()
 {
-	tft.setOrientation(Orientation::deg270);
 	auto size = tft.getSize();
 	auto scene = new SceneObject(size, F("Color Tests"));
 	scene->clear();
 
-	Color color[] = {Color::RED,  Color::GREEN, Color::BLUE, Color::WHITE};
+	Color color[] = {Color::RED, Color::GREEN, Color::BLUE, Color::WHITE};
 	uint8_t numColors = ARRAY_SIZE(color);
 
-	auto height = size.h;
+	auto height = size.h / 4;
 	auto width = size.w / numColors;
 
-	for(unsigned i=0; i< numColors; i++) {
-		scene->fillRect(i * width, 0, width, height, color[i]);
+	TextBuilder text(*scene);
+	text.setColor(Color::White, Color::Black);
+	text.setScale(2);
+	text.setTextAlign(Align::Centre);
+	text.setLineAlign(Align::Centre);
+	for(unsigned i = 0; i < numColors; i++) {
+		for(unsigned j = 0; j < 4; j++) {
+			PixelBuffer pix{color[i]};
+			auto scale = [&](uint8_t& c) { c = c * (j + 1) / 5; };
+			scale(pix.bgr24.r);
+			scale(pix.bgr24.g);
+			scale(pix.bgr24.b);
+			Rect r(i * width, j * height, width, height);
+			scene->fillRect(pix.color, r);
+
+			text.setClip(r);
+			text.printf("%c%c", 'A' + i, '0' + j);
+		}
 	}
+	text.commit(*scene);
 
 	renderQueue.render(scene, done);
 }
@@ -45,7 +60,6 @@ void init()
 {
 	Serial.begin(SERIAL_BAUD_RATE); // 115200 by default
 	Serial.systemDebugOutput(true); // Allow debug output to serial
-
 
 #ifdef ARCH_HOST
 	setDigitalHooks(nullptr);
