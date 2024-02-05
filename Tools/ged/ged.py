bc6d4766118b7190c78896c4813e50ac167d9805
mikee47 <mike@silyhouse.net>
2024-02-05 12:01:56 +0000

    Move adjust logic out of element, resize does the specifics

--- Tools/ged/ged.py
+++ Tools/ged/ged.py
@@ -1,8 +1,9 @@
 import os
 import sys
 import copy
-from enum import IntEnum
+from enum import Enum, IntEnum
 from random import randrange
+from dataclasses import dataclass
 import tkinter as tk
 
 
@@ -20,30 +21,14 @@ def align(value):
     n = (value + a // 2) // a
     return n * a
 
-
-class Rect:
-    def __init__(self, x=0, y=0, w=0, h=0):
-        self.x = x
-        self.y = y
-        self.w = w
-        self.h = h
-
-    def inflate(self, off):
-        return Rect(self.x - off[0], self.y - off[1], self.w + off[0]*2, self.h + off[1]*2)
-
-    def bounds(self):
-        return (self.x, self.y, self.x + self.w, self.y + self.h)
-
-
 # Top 4 bits identify hit class
-HIT_CLASS_MASK = 0xf0
+ELEMENT_CLASS_MASK = 0xf0
 # Lower 4 bits for direction
-HIT_DIR_MASK = 0x0f
+ELEMENT_DIR_MASK = 0x0f
 # Hit mask for handles at corners and midpoints of bounding rect
-HIT_HANDLE = 0x10
-# Hit mask for edges of bounding rectangle
-HIT_EDGE = 0x20
-HIT_BODY = 0x30
+EC_HANDLE = 0x10
+# Hit mask for item elements
+EC_ITEM = 0x20
 
 # Directions
 DIR_N = 0x01
@@ -55,108 +40,93 @@ DIR_SE = DIR_S | DIR_E
 DIR_SW = DIR_S | DIR_W
 DIR_NW = DIR_N | DIR_W
 
-def hit_class(e):
-    return e & HIT_CLASS_MASK
-
-def hit_dir(e):
-    return e & HIT_DIR_MASK
-
 class Element(IntEnum):
-    HANDLE_N = HIT_HANDLE | DIR_N
-    HANDLE_NE = HIT_HANDLE | DIR_NE
-    HANDLE_E = HIT_HANDLE | DIR_E
-    HANDLE_SE = HIT_HANDLE | DIR_SE
-    HANDLE_S = HIT_HANDLE | DIR_S
-    HANDLE_SW = HIT_HANDLE | DIR_SW
-    HANDLE_W = HIT_HANDLE | DIR_W
-    HANDLE_NW = HIT_HANDLE | DIR_NW
+    HANDLE_N = EC_HANDLE | DIR_N
+    HANDLE_NE = EC_HANDLE | DIR_NE
+    HANDLE_E = EC_HANDLE | DIR_E
+    HANDLE_SE = EC_HANDLE | DIR_SE
+    HANDLE_S = EC_HANDLE | DIR_S
+    HANDLE_SW = EC_HANDLE | DIR_SW
+    HANDLE_W = EC_HANDLE | DIR_W
+    HANDLE_NW = EC_HANDLE | DIR_NW
 
-    BODY = HIT_BODY
+    ITEM = EC_ITEM | 0x01
+
+    def is_handle(self):
+        return (self & ELEMENT_CLASS_MASK) == EC_HANDLE
 
 
-class GElement(Rect):
-    def __init__(self, x=0, y=0, w=0, h=0):
-        super().__init__(x, y, w, h)
-        self.init()
+@dataclass
+class Rect:
+    x: int = 0
+    y: int = 0
+    w: int = 0
+    h: int = 0
 
-    def init(self):
-        self.line_width = 1
-        self.color = 0xa0a0a0
+    def set_rect(self, rect):
+        self.x, self.y, self.w, self.h = rect.x, rect.y, rect.w, rect.h
 
-    def get_tag(self):
-        return 'G%08x' % id(self)
+    def align(self):
+        return Rect(align(self.x), align(self.y), align(self.w), align(self.h))
 
-    def get_cursor(self, elem):
-        if elem is None:
-            return None
+    def inflate(self, off):
+        return Rect(self.x - off[0], self.y - off[1], self.w + off[0]*2, self.h + off[1]*2)
+
+    def handle_pos(self, elem: Element):
         return {
-            Element.HANDLE_NW: 'top_left_corner',
-            Element.HANDLE_SE: 'bottom_right_corner',
-            Element.HANDLE_NE: 'top_right_corner',
-            Element.HANDLE_SW: 'bottom_left_corner',
-            Element.HANDLE_N: 'sb_v_double_arrow',
-            Element.HANDLE_S: 'sb_v_double_arrow',
-            Element.HANDLE_E: 'sb_h_double_arrow',
-            Element.HANDLE_W: 'sb_h_double_arrow',
-            Element.BODY: 'target',
+            Element.HANDLE_N: (self.x + self.w // 2, self.y),
+            Element.HANDLE_NE: (self.x + self.w, self.y),
+            Element.HANDLE_E: (self.x + self.w, self.y + self.h // 2),
+            Element.HANDLE_SE: (self.x + self.w, self.y + self.h),
+            Element.HANDLE_S: (self.x + self.w // 2, self.y + self.h),
+            Element.HANDLE_SW: (self.x, self.y + self.h),
+            Element.HANDLE_W: (self.x, self.y + self.h // 2),
+            Element.HANDLE_NW: (self.x, self.y),
         }.get(elem)
-
-    def remove_handles(self, canvas):
-        canvas.delete('handle')
-
-    def draw_handles(self, canvas):
-        for e in Element:
-            if hit_class(e) != HIT_HANDLE:
-                continue
-            r = self.element_rect(e).bounds()
-            tags = (self.get_tag(), str(e), 'handle')
-            canvas.create_rectangle(r[0], r[1], r[2], r[3], outline='white', width=1, tags=tags)
 
     def element_rect(self, elem):
         HR = 4, 4
         ER = 1, 1
-        return {
-            Element.HANDLE_N: Rect(self.x + self.w // 2, self.y, 1, 1).inflate(HR),
-            Element.HANDLE_NE: Rect(self.x + self.w, self.y, 1, 1).inflate(HR),
-            Element.HANDLE_E: Rect(self.x + self.w, self.y + self.h // 2, 1, 1).inflate(HR),
-            Element.HANDLE_SE: Rect(self.x + self.w, self.y + self.h, 1, 1).inflate(HR),
-            Element.HANDLE_S: Rect(self.x + self.w // 2, self.y + self.h, 1, 1).inflate(HR),
-            Element.HANDLE_SW: Rect(self.x, self.y + self.h, 1, 1).inflate(HR),
-            Element.HANDLE_W: Rect(self.x, self.y + self.h // 2, 1, 1).inflate(HR),
-            Element.HANDLE_NW: Rect(self.x, self.y, 1, 1).inflate(HR),
-            Element.BODY: Rect(self.x, self.y, self.w, self.h).inflate(ER),
-        }.get(elem)
+        if elem == Element.ITEM:
+            return Rect(self.x, self.y, self.w, self.h).inflate(ER)
+        pt = self.handle_pos(elem)
+        return Rect(pt[0], pt[1], 1, 1).inflate(HR)
+
+    def tk_bounds(self):
+        return (self.x, self.y, self.x + self.w, self.y + self.h)
+
+
+class GItem(Rect):
+    def __init__(self, bounds: Rect):
+        super().__init__(bounds.x, bounds.y, bounds.w, bounds.h)
+
+    def __post_init__(self):
+        super().__post_init__()
+        self.line_width = 1
+        self.color = 0xa0a0a0
+
+    def get_bounds(self):
+        return Rect(self.x, self.y, self.w, self.h)
+
+    def get_tag(self):
+        return 'G%08x' % id(self)
+
+    def get_item_tags(self):
+        return ('item', str(Element.ITEM), self.get_tag())
 
     def get_min_size(self):
         o = self.line_width * 2
         return (MIN_ELEMENT_WIDTH + o, MIN_ELEMENT_HEIGHT + o)
 
-    def adjust(self, canvas, elem, orig, off):
-        x, y, w, h = self.x, self.y, self.w, self.h
-        if elem == Element.BODY:
-            x, y = orig.x + off[0], orig.y + off[1]
-        else:
-            if elem & DIR_N:
-                y, h = orig.y + off[1], orig.h - off[1]
-            if elem & DIR_E:
-                w = orig.w + off[0]
-            if elem & DIR_S:
-                h = orig.h + off[1]
-            if elem & DIR_W:
-                x, w = orig.x + off[0], orig.w - off[0]
-            min_size = self.get_min_size()
-            w, h = max(w, min_size[0]), max(h, min_size[1])
-        self.resize(canvas, x, y, w, h)
-
-    def resize(self, canvas, x, y, w, h):
-        self.x, self.y, self.w, self.h = align((x, y, w, h))
+    def resize(self, canvas, rect):
+        self.set_rect(rect)
         canvas.delete(self.get_tag())
         self.draw(canvas)
 
 
-class GRect(GElement):
-    def init(self):
-        super().init()
+class GRect(GItem):
+    def __post_init__(self):
+        super().__post_init__()
         self.radius = 0
 
     def get_min_size(self):
@@ -166,10 +136,10 @@ class GRect(GElement):
 
     def draw(self, canvas):
         # pygame.draw.rect(surface, self.color, self, self.line_width, self.radius)
-        x1, y1, x2, y2 = self.bounds()
+        x1, y1, x2, y2 = self.tk_bounds()
         w = self.line_width
         color = '#%06x' % self.color
-        tags = (self.get_tag(), str(Element.BODY), 'item')
+        tags = self.get_item_tags()
 
         def draw_rect(x0, y0, x1, y1):
             canvas.create_rectangle(x0, y0, x1, y1, outline=color, width=w, tags=tags)
@@ -207,17 +177,21 @@ class GRect(GElement):
             draw_rect(x1, y1, x2, y2)
 
 
-class GEllipse(GElement):
+class GEllipse(GItem):
     def draw(self, canvas):
         # pygame.draw.ellipse(surface, self.color, self, self.line_width)
-        x1, y1, x2, y2 = self.bounds()
+        x1, y1, x2, y2 = self.tk_bounds()
         color = '#%06x' % self.color
-        tags = (self.get_tag(), str(Element.BODY), 'item')
+        tags = self.get_item_tags()
         if self.line_width == 0:
             canvas.create_oval(x1, y1, x2, y2, fill=color, tags=tags)
         else:
             canvas.create_oval(x1, y1, x2, y2, outline=color, width=self.line_width, tags=tags)
 
+class State(Enum):
+    IDLE = 0
+    DRAGGING = 1
+    SELECTING = 2
 
 class Handler:
     def __init__(self, tk_root, size):
@@ -229,7 +203,7 @@ class Handler:
         c.bind('<ButtonRelease-1>', self.canvas_end_move)
         self.display_list = []
         self.sel_item = None
-        self.is_dragging = False
+        self.state = State.IDLE
 
     def add_random_shapes(self, count):
         self.display_list = []
@@ -240,12 +214,12 @@ class Handler:
             h = randrange(h_min, h_max)
             x = randrange(DISPLAY_WIDTH - w)
             y = randrange(DISPLAY_HEIGHT - h)
-            x, y, w, h = align(x), align(y), align(w), align(h)
+            r = Rect(x, y, w, h).align()
             kind = randrange(5)
             if kind == 1:
-                item = GEllipse(x, y, w, h)
+                item = GEllipse(r)
             else:
-                item = GRect(x, y, w, h)
+                item = GRect(r)
                 item.radius = randrange(0, min(w,h)//2)
             item.color = randrange(0xffffff)
             item.line_width = randrange(5)
@@ -263,54 +237,97 @@ class Handler:
         tags = self.canvas.gettags('current')
         if not tags:
             return None, None
-        item = next(x for x in self.display_list if x.get_tag() == tags[0])
         elem = Element(int(tags[1]))
-        return item, elem
+        if elem == Element.ITEM:
+            item = next(x for x in self.display_list if x.get_tag() == tags[2])
+        else:
+            item = None
+        return elem, item
+
+    def draw_handles(self, rect):
+        for e in Element:
+            if not e.is_handle():
+                continue
+            r = rect.element_rect(e).tk_bounds()
+            tags = ('handle', str(e))
+            self.canvas.create_rectangle(r[0], r[1], r[2], r[3], outline='white', width=1, tags=tags)
+
+    def remove_handles(self):
+        self.canvas.delete('handle')
 
     def canvas_select(self, evt):
-        item, elem = self.get_current()
-        if not item:
+        elem, item = self.get_current()
+        if not elem:
             if self.sel_item:
-                self.sel_item.remove_handles(self.canvas)
+                self.remove_handles()
                 self.sel_item = None
             return
 
         self.sel_pos = (evt.x, evt.y)
-        self.orig_item = copy.copy(item)
         self.sel_elem = elem
-        if item != self.sel_item:
-            if self.sel_item:
-                self.sel_item.remove_handles(self.canvas)
-            item.draw_handles(self.canvas)
+        if item and item != self.sel_item:
+            self.remove_handles()
+            self.draw_handles(item)
             self.sel_item = item
+        self.orig_item = copy.copy(self.sel_item)
+
+    @staticmethod
+    def get_cursor(elem):
+        if elem is None:
+            return ''
+        return {
+            Element.HANDLE_NW: 'top_left_corner',
+            Element.HANDLE_SE: 'bottom_right_corner',
+            Element.HANDLE_NE: 'top_right_corner',
+            Element.HANDLE_SW: 'bottom_left_corner',
+            Element.HANDLE_N: 'sb_v_double_arrow',
+            Element.HANDLE_S: 'sb_v_double_arrow',
+            Element.HANDLE_E: 'sb_h_double_arrow',
+            Element.HANDLE_W: 'sb_h_double_arrow',
+            Element.ITEM: 'target',
+        }.get(elem)
 
     def canvas_move(self, evt):
-        if self.is_dragging:
+        if self.state == State.DRAGGING:
             return
-        item, elem = self.get_current()
-        if not item:
-            self.canvas.configure({'cursor': ''})
-            return
-        csr = item.get_cursor(elem)
-        if csr:
-            self.canvas.configure({'cursor': csr})
+        elem, item = self.get_current()
+        self.canvas.configure({'cursor': self.get_cursor(elem)})
 
     def canvas_drag(self, evt):
-        e = self.sel_item
-        if not e:
+        item = self.sel_item
+        if not item:
             return
-        if not self.is_dragging:
-            e.remove_handles(self.canvas)
-            self.is_dragging = True
-        e.adjust(self.canvas, self.sel_elem, self.orig_item, (evt.x - self.sel_pos[0], evt.y - self.sel_pos[1]))
+        if self.state != State.DRAGGING:
+            self.remove_handles()
+            self.state = State.DRAGGING
+        elem = self.sel_elem
+        orig = self.orig_item
+        r = item.get_bounds()
+        off = evt.x - self.sel_pos[0], evt.y - self.sel_pos[1]
+        if elem == Element.ITEM:
+            r.x, r.y = align((orig.x + off[0], orig.y + off[1]))
+        else:
+            if elem & DIR_N:
+                r.y = align(orig.y + off[1])
+                r.h = orig.y + orig.h - r.y
+            if elem & DIR_E:
+                r.w = align(orig.w + off[0])
+            if elem & DIR_S:
+                r.h = align(orig.h + off[1])
+            if elem & DIR_W:
+                r.x = align(orig.x + off[0])
+                r.w = orig.x + orig.w - r.x
+            min_size = item.get_min_size()
+            if r.w < min_size[0] or r.h < min_size[1]:
+                return
+        item.resize(self.canvas, r)
 
     def canvas_end_move(self, evt):
-        if not self.is_dragging:
+        if self.state != State.DRAGGING:
             return
-        self.is_dragging = False
-        e = self.sel_item
-        e.remove_handles(self.canvas)
-        e.draw_handles(self.canvas)
+        self.state = State.IDLE
+        self.remove_handles()
+        self.draw_handles(self.sel_item)
 
 
 def run():
