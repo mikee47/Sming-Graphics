mikee47/Sming-Graphics

commit 89d315bdaf45ea9b75692a8f21e07376fa6d72ba

`abs` isnt't constexpr in clang

src/include/Graphics/Drawing/Macros.h
@@ -44,7 +44,7 @@
 #define GDRAW_TYPE(type) GDRAW_FIELD(Header::type, 4)
 #define GDRAW_CMD(cmd) GDRAW_OPCODE(OpCode::execute) | GDRAW_TYPE(Type::uint8) | uint8_t(Graphics::Drawing::cmd),
 #define GDRAW_UINT16(value) uint8_t(value), uint8_t((value) >> 8),
-#define GDRAW_INT16(value) uint8_t(abs(value)), uint8_t(abs(value) >> 8),
+#define GDRAW_INT16(value) uint8_t(value), uint8_t(uint16_t(value) >> 8),
 #define GDRAW_UINT32(value) uint8_t(value), uint8_t((value) >> 8), uint8_t((value) >> 16), uint8_t((value) >> 24),
 #define GDRAW_MAKE_UINT32(w1, w2) (uint32_t(w2) << 16) | uint32_t(w1)
 #define GDRAW_REGDEF(reg, size) uint8_t(offsetof(Graphics::Drawing::Registers, reg) / size)

commit 351fedf3e4944fb51f50a32647d85017d97198a3

Fix abs() bug in drawing macros
Introduced in commit 89d315bd because clang doesn't provide constexpr abs().

src/include/Graphics/Drawing/Macros.h
@@ -39,12 +39,18 @@
  *   )
  */
 
+// GCC could just use `abs()`, but it isn't constexpr for clang
+inline constexpr uint16_t gdrawAbs(int16_t value)
+{
+	return (value < 0) ? -value : value;
+}
+
 #define GDRAW_FIELD(value, shift) uint8_t(uint8_t(Graphics::Drawing::value) << shift)
 #define GDRAW_OPCODE(opcode) GDRAW_FIELD(opcode, 6)
 #define GDRAW_TYPE(type) GDRAW_FIELD(Header::type, 4)
 #define GDRAW_CMD(cmd) GDRAW_OPCODE(OpCode::execute) | GDRAW_TYPE(Type::uint8) | uint8_t(Graphics::Drawing::cmd),
 #define GDRAW_UINT16(value) uint8_t(value), uint8_t((value) >> 8),
-#define GDRAW_INT16(value) uint8_t(value), uint8_t(uint16_t(value) >> 8),
+#define GDRAW_INT16(value) uint8_t(gdrawAbs(value)), uint8_t((value) / 256),
 #define GDRAW_UINT32(value) uint8_t(value), uint8_t((value) >> 8), uint8_t((value) >> 16), uint8_t((value) >> 24),
 #define GDRAW_MAKE_UINT32(w1, w2) (uint32_t(w2) << 16) | uint32_t(w1)
 #define GDRAW_REGDEF(reg, size) uint8_t(offsetof(Graphics::Drawing::Registers, reg) / size)